e7b5ce2b9259b40cfeaec9c2e20125f61b3c4a40
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-11-12 11:05:09 +0000

    Add message if no ingredients have been added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import "./Burger.css"
 
 const Burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingredientKey => {
             return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
                 return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
             })
         })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, [])
+    
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
     return (
         <div className="burger">
