9315fa89ad095429dd83a1fe63566e57716de648
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-11-12 11:25:15 +0000

    Build control buttons added

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,24 @@
+import React from "react"
+import BuildControl from "./BuildControl/BuildControl"
+import "./BuildControls.css"
+
+const controls = [
+    { label: "Salad", type: "salad" },
+    { label: "Bacon", type: "bacon" },
+    { label: "Cheese", type: "cheese" },
+    { label: "Meat", type: "meat" }
+]
+
+const BuildControls = (props) => {
+
+    return (
+        <div className="buildControls">
+            {controls.map(ctrl => (
+                <BuildControl key={ctrl.label} label={ctrl.label} type={ctrl.type} />
+            ))}
+        </div>
+    )
+    
+}
+
+export default BuildControls
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import Burger from "../../components/Burger/Burger"
+import BuildControls from "../../components/Burger/BuildControls/BuildControls"
 
 import Aux from "../../hoc/aux"
 
@@ -18,7 +19,7 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls />
             </Aux>
         )
     }
