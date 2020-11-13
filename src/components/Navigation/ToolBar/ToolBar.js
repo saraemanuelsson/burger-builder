5632f7488fa6905573db88a33a04d91a5c016485
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-11-13 14:38:13 +0000

    Replaced menu div with menu icon which will open the sidedrawer

--- src/components/Navigation/ToolBar/ToolBar.js
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -2,12 +2,13 @@ import React from "react"
 import "./ToolBar.css"
 import Logo from "../../Logo/Logo"
 import NavigationItems from "../NavigationItems/NavigationItems"
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle"
 
 const ToolBar = (props) => {
 
     return (
         <header className="toolBar">
-            <div>MENU</div>
+            <DrawerToggle clicked={props.drawerToggleClicked}/>
             <Logo height="80%"/>
             <nav className="desktopOnly">
                 <NavigationItems />
